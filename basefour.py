af65fa7be1d26b7d8d26d9656a61a2fc0abfedfc
Erik McLaughlin <erikm6872@gmail.com>
2015-11-18 19:53:02 -0700

    Moved base10 -> base 4 calculations to separate method

--- basefour.py
+++ basefour.py
@@ -6,12 +6,17 @@ import math
 
 class BaseFour(object):
     def __init__(self, dec):
-        self.baseFourVals = []  #Array containing base 4 value
-        while dec > 3:  #dec := result
-            self.baseFourVals.insert(0,dec%4)   #Insert remainder into array
-            dec = dec / 4                       #Update dec
-        self.baseFourVals.insert(0,dec) #Insert last value
+        self.baseFourVals = self.fromDecimal(dec)   #Array containing base 4 value
         print self.baseFourVals #Print entire array (testing)
+    
+    #Convert from decimal to base 4
+    def fromDecimal(self, dec):
+        retArr = []  #Array containing base 4 value
+        while dec > 3:  #dec := result
+            retArr.insert(0,dec%4)   #Insert remainder into array
+            dec = dec / 4                       #Update dec
+        retArr.insert(0,dec) #Insert last value
+        return retArr
     
     #Convert base 4 value back to base 10
     def toDecimal(self):
@@ -19,3 +24,4 @@ class BaseFour(object):
         for i in range(len(self.baseFourVals)):
             retDec = retDec + (self.baseFourVals[i] * math.pow(4, len(self.baseFourVals) - i - 1))
         return int(retDec)
+        
