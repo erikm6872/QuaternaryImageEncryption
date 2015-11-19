cadea5c9c1ad2212ecf43619afe0f35322cb05ad
Erik McLaughlin <erikm6872@gmail.com>
2015-11-18 18:15:10 -0700

    Imports

--- decrypt.py
+++ decrypt.py
@@ -2,6 +2,9 @@
 #11/18/2015
 #https://github.com/erikm6872/QuaternaryImageEncryption
 
+#Imports
+from PIL import Image
+
 def main():
 
 main()
--- encrypt.py
+++ encrypt.py
@@ -2,6 +2,7 @@
 #11/18/2015
 #https://github.com/erikm6872/QuaternaryImageEncryption
 
+#Imports
 from PIL import Image
 from random import *
 
