8f54ab811febee7eda5561255e3f4f1acb53a7ce
Erik McLaughlin <erikm6872@gmail.com>
2015-11-19 11:13:47 -0700

    Added functionality to pass filename as argument

--- main.py
+++ main.py
@@ -3,17 +3,23 @@
 #https://github.com/erikm6872/QuaternaryImageEncryption
 
 #Imports
+import sys
 from encrypt import *
 from decrypt import *
-def main():
 
-    #Get filename to open - sample.jpg hardcoded for testing purposes
+def main():
     
-    #fname = raw_input("fname=")
-    fname = 'sample.jpg' 
-    
+    #Set filename if passed as an argument
+    if len(sys.argv) == 2:
+        fname = sys.argv[1] 
+    else:
+        print "Usage: image.py <filename>"
+        
+        #Get filename to open - sample.jpg hardcoded for testing purposes
+        fname = raw_input("fname=")
+        #fname = 'sample.jpg' 
+
     encryptedfname = encrypt(fname)
     decryptedfname = decrypt(fname)
-    
     
 main()
